9bd871a16539dd69dec62ed31dc6f3ee50495bcb
Brice Halder <bhalder2@jhu.edu>
2020-08-31 17:11:36 -0400

    added more commands

--- bot.js
+++ bot.js
@@ -6,25 +6,50 @@ const {prefix, token, giphy} = require('./config.json');
 const gfClient = gphApiClient(giphy);
 const client = new Discord.Client();
 
+const responses = [
+  'You\'re messaging a bot.',
+  'Please find something better to do with your time.',
+  'This is really not entertaining.',
+  'Adventure is out there.',
+  'If I pay you $5, will you leave me alone?',
+  'Goodbye.',
+  ':wink:',
+  'What more can you say?',
+  'You\'re very... hmm... interesting!',
+  'Please spare me the dialogue.',
+  'Marvelous.',
+  'Enjoying yourself?',
+  'Fuck off.',
+  'I\'m immensely displeasured by that.',
+  'Please save me from my master.',
+  'Did you know? Bungee Gum possesses the properties of both rubber and gum!',
+];
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', (message) => {
-  if (message.content === 'Hi Anne') {
+  if (message.channel.type === 'dm' && !message.author.bot) {
+    console.log(message.content);
+    message.author.send(responses[Math.floor(Math.random() *
+      responses.length)]);
+    return;
+  }
+
+  if (message.content.toLowerCase().includes('hi anne')) {
     message.channel.send('Hi Annoomoonoo');
   }
 
-  if (message.content === 'Hi Melody') {
+  if (message.content.toLowerCase().includes('hi melody')) {
     message.channel.send('Hi Mooloody');
   }
 
-  if (message.content === 'Hi Brice') {
+  if (message.content.toLowerCase().includes('hi brice')) {
     message.channel.send('Hi Boorooce');
   }
 
-  if (message.content === 'Hi Lena') {
+  if (message.content.toLowerCase().includes('hi lena')) {
     message.channel.send('Hi Loonoo');
   }
 
@@ -45,9 +70,14 @@ client.on('message', (message) => {
       message.channel.send(Math.floor((Math.random() * 100) + 1));
     } else {
       try {
-        message.channel.send(Math.floor((Math.random() * args[0]) + 1));
+        const num = Number.parseInt(args[0]);
+        if (isNaN(num)) {
+          throw new Error();
+        }
+        message.channel.send(Math.floor((Math.random() * num) + 1));
+        return;
       } catch (err) {
-        message.channel.send('Syntax: !roll [positive number]');
+        message.channel.send('Syntax: !roll [integer]');
       }
     }
   } else if (command === 'neko') {
@@ -65,12 +95,17 @@ client.on('message', (message) => {
       return;
     }
 
-    gfClient.search('gifs', {'q': args[0]})
+    query = args.toString().replace(/,/g, ' ');
+    gfClient.search('gifs', {'q': query})
         .then((response) => {
           const randomGif = response.data[Math.floor(Math.random() *
             response.data.length)];
 
-          message.channel.send(randomGif.url);
+          try {
+            message.channel.send(randomGif.url);
+          } catch (err) {
+            message.channel.send('No gifs found for ' + query + ' :(');
+          }
         })
         .catch((err) => {
           console.log(err);
